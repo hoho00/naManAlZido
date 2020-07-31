2b4a023547d2db94c8d3041e93159e654d48157e
hoho00 <jhoya963@gmail.com>
2020-07-31 15:28:31 +0900

    자동로그인 및 인트로 이미지 구현

---
+++ app/src/main/java/com/hackerton/googlemap/model/MapItem.java
@@ -0,0 +1,39 @@
+package com.hackerton.googlemap.model;
+
+public class MapItem {
+    private double latitude;
+    private double longitude;
+    private String address;
+
+    public MapItem(){}
+
+    public MapItem(double latitude, double longitude, String address) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
